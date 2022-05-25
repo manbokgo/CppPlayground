d86e8b8f6d71bd7d46fae1f549f66b073884b723
manbokgo <manbok285@gmail.com>
2022-05-25 16:14:35 +0900

    컴파일타임 튜플 매크로 오..

---
+++ CppPlayground/compile-time tuple macro.cpp
@@ -0,0 +1,28 @@
+﻿#include <cstdio>
+
+// id(int), "name"(const char*), hp(int)
+#define MONSTER_DATA					 	\
+    MONSTER_ENTRY(0, "pope",	100)		\
+    MONSTER_ENTRY(1, "big rat",	30)			\
+    MONSTER_ENTRY(2, "mama",	255)		\
+    MONSTER_ENTRY(3, "dragon",	300000)		\
+
+int main(void)
+{
+	int ids[] = {
+		#define MONSTER_ENTRY(id, name, hp) id,
+		MONSTER_DATA
+		#undef    MONSTER_ENTRY
+	};
+
+	const char* names[] = {
+		#define MONSTER_ENTRY(id, name, hp) name,
+		MONSTER_DATA
+		#undef    MONSTER_ENTRY
+	};
+
+	for (size_t i = 0; i < sizeof(ids) / sizeof(int); ++i)
+	{
+		printf("%3d %s\n", ids[i], names[i]);
+	}
+};
