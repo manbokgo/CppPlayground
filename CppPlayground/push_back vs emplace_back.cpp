c8779ad591cdb96dfaa21f9b260138dd070b7fe1
manbokgo <manbok285@gmail.com>
2022-05-27 19:20:19 +0900

    emplace_back 비교

---
+++ CppPlayground/push_back vs emplace_back.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <string>
+#include <utility>
+
+namespace {
+    //*
+    char const A[] = "Bob";
+    char const B[] = "pizza";
+    char const C[] = "Smith";
+    /*/
+    char const A[] = "Some long string that doesn't fit in SSO";
+    char const B[] = "Some other long string that doesn't fit in SSO";
+    char const C[] = "Yet another long string that doesn't fit in SSO";
+    //*/
+}
+
+int main()
+{
+	{
+		std::vector<std::string> vec;
+		vec.reserve(1);
+		vec.push_back(A);
+	}
+
+    {
+        std::vector<std::string> vec;
+        vec.reserve(1);
+        vec.emplace_back(A);
+    }
+
+}
+
