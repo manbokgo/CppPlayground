0a22ed458a4ee81b0e0226f9a55919cbbeed3b40
manbokgo <manbok285@gmail.com>
2022-05-28 21:59:45 +0900

    c++20 NumAggregateMembers

---
+++ CppPlayground/play with c++20.cpp
@@ -0,0 +1,45 @@
+﻿// 오... 근데 이거 디버깅은 못하나
+
+#include <utility>
+
+
+template <size_t>
+struct Any
+{
+	template <class T>
+	operator T() const;
+};
+
+
+template <class T, size_t N>
+concept ConstructibleWithN = requires
+{
+	[]<size_t...Idxs>(std::index_sequence<Idxs...>) -> decltype(T{Any<Idxs>{}...})
+	{
+		return {};
+	}(std::make_index_sequence<N>{});
+};
+
+template <class T, size_t N>
+concept AggregateOfN = std::is_aggregate_v<T> && ConstructibleWithN<T, N> && !ConstructibleWithN<T, N + 1>;
+
+template <class T>
+constexpr auto NumAggregateMembers = []<size_t...Idxs>(std::index_sequence<Idxs...>)
+{
+	return ((AggregateOfN<T, Idxs> * Idxs) + ... + 0);
+}(std::make_index_sequence<32>{});
+
+
+struct Foo
+{
+	int    i;
+	double j;
+	int x = 3;
+	int y;
+};
+
+
+int main()
+{
+	static_assert(NumAggregateMembers<Foo> == 2);
+}
