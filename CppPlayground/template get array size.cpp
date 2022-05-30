02eb96589dea3f3bde0da6412199be4a1677a46b
manbokgo <manbok285@gmail.com>
2022-05-30 19:11:02 +0900

    template get array size

---
+++ CppPlayground/template get array size.cpp
@@ -0,0 +1,22 @@
+﻿template <typename T, size_t size>
+[[nodiscard]] constexpr size_t GetArrLength(const T(&)[size]) { return size; }
+
+struct Test
+{
+	int a[2];
+
+	[[nodiscard]] constexpr size_t Foo() const
+	{
+		return GetArrLength(a);
+	}
+};
+
+
+int main()
+{
+	int a[] = { 1,2,3,4 };
+	GetArrLength(a);
+
+	Test t{};
+	size_t size = t.Foo();
+}
