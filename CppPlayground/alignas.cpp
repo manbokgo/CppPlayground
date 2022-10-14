67af9a2f8e4b7448520f2dcf232e39e49e176aa1
manbokgo <manbok285@gmail.com>
2022-10-15 00:23:39 +0900

    갸아악

---
+++ CppPlayground/alignas.cpp
@@ -0,0 +1,42 @@
+﻿struct XMFLOAT4
+{
+    float data[4];
+};
+struct XMFLOAT3
+{
+    float data[3];
+};
+struct XMFLOAT2
+{
+    float data;
+    float data2;
+};
+
+#pragma pack(4)
+struct Foo
+{
+    char a;
+    int b;
+    char c;
+    double d;
+};
+
+#include <iostream>
+
+int main()
+{
+    Foo x, y, z;
+    std::cout
+        << "sizeof(Foo) = " << sizeof(Foo) << '\n'
+        << "alignof(Foo) = " << alignof(Foo) << '\n'
+        << std::hex << std::showbase
+        << "&x: " << &x << '\n'
+        << "&y: " << &y << '\n'
+        << "&z: " << &z << '\n'
+        << std::dec << std::showbase
+        << "&diff: " << (int)&z - (int)&y << '\n'
+        << "&z a: " << offsetof(Foo, a) << '\n'
+        << "&z b: " << offsetof(Foo, b) << '\n'
+        << "&z c: " << offsetof(Foo, c) << '\n'
+        << "&z d: " << offsetof(Foo, d) << '\n';
+}
