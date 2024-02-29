65df492bbd8de5b200ce33590013463473bb3e1d
manbokgo <manbok285@gmail.com>
2024-02-29 17:55:54 +0900

    1520 DP, DFS

--- BOJ/problem/1520 내리막 길.cpp
+++ BOJ/problem/1520 내리막 길.cpp
@@ -1,11 +1,16 @@
 ﻿// URL: https://www.acmicpc.net/problem/1520
-// Algo: 
+// Algo: DP, DFS
 
 // Start:	240229 17 30
 // Read:	0 0
 // Think:	0 2
-// Code:	0 4 시간초과
-// Total:	
+// Code:	0 4 DFS 시간초과
+// Code:	0 10 +DP 시간초과
+// Code:	0 5 dp 초기값 -1로 변경
+// Total:	0 21
+
+// 한번 방문한 타일도 재방문할 수 있으니 DFS 시간복잡도가 O(5V)가 아니라 최소 O(V^2)이다.
+// 그냥 DFS 만으로는 풀 수 없고 DP가 들어가야함. 미리 좀 생각하고 풀자.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -35,15 +40,14 @@ int dx[4] = {1, 0, -1, 0}; // 동남서북
 bool OOB(int y, int x) { return y < 0 || y >= n || x < 0 || x >= m; }
 
 int tile[505][505];
-int answer = 0;
-void dfs(int y, int x)
-{
-    if (y == n - 1 && x == m - 1)
-    {
-        ++answer;
-        return;
-    }
+int dp[505][505];
 
+int dfs(int y, int x)
+{
+    // 실수: dp=0 이어도 바로 리턴해야함
+    if (dp[y][x] >= 0) return dp[y][x];
+
+    dp[y][x] = 0;
     for (int i = 0; i < 4; ++i)
     {
         int ny = y + dy[i];
@@ -51,8 +55,10 @@ void dfs(int y, int x)
         if (OOB(ny, nx)) continue;
         if (tile[y][x] <= tile[ny][nx]) continue;
 
-        dfs(ny, nx);
+        dp[y][x] += dfs(ny, nx);
     }
+
+    return dp[y][x];
 }
 
 int main()
@@ -69,6 +75,8 @@ int main()
         }
     }
 
+    memset(dp, -1, sizeof(dp));
+    dp[n-1][m-1] = 1;
     dfs(0, 0);
-    cout << answer;
+    cout << dp[0][0];
 }
