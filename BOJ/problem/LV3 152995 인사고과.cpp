117ca755eed62d502c4f3f1b0dd44445dd81780c
manbokgo <manbok285@gmail.com>
2024-04-02 19:05:19 +0900

    152995 푸는중

--- BOJ/problem/LV3 152995 인사고과.cpp
+++ BOJ/problem/LV3 152995 인사고과.cpp
@@ -15,44 +15,99 @@ using pii = pair<int, int>;
 constexpr int INF = 0x3f3f3f3f;
 
 int solution(vector<vector<int>> scores) {
-    vector<pii> sums;
+    pii minA = {INF, INF};
+    pii minB = {INF, INF};
+    
+    vector<pii> tmp, sums;
+    tmp.reserve(scores.size());
     sums.reserve(scores.size());
     
     for (int i = 0; i < scores.size(); ++i)
     {
         const auto& score = scores[i];
+        tmp.push_back({score[0], score[1]});
         sums.push_back({score[0] + score[1], i});
     }
-    
     sort(sums.begin(), sums.end(), greater<>());
     
+    sort(tmp.begin(), tmp.end(), [](const auto& a, const auto& b) { return a.first < b.first; });
+    for (int i = 0; i < tmp.size(); ++i)
+    {
+        auto [a, b] = tmp[i];
+        bool isMin = true;
+        for (int j = i + 1; j < tmp.size(); ++j)
+        {
+            auto [na, nb] = tmp[j];
+            if (a < na && b < nb)
+            {
+                isMin = false;
+                i = j;
+                break;
+            }
+        }
+        
+        if (isMin)
+        {
+            minA = tmp[i];
+            break;
+        }
+    }
+    
+    sort(tmp.begin(), tmp.end(), [](const auto& a, const auto& b) { return a.second < b.second; });
+    for (int i = 0; i < tmp.size(); ++i)
+    {
+        auto [a, b] = tmp[i];
+        bool isMin = true;
+        for (int j = i + 1; j < tmp.size(); ++j)
+        {
+            auto [na, nb] = tmp[j];
+            if (b < nb && a < na)
+            {
+                isMin = false;
+                i = j;
+                break;
+            }
+        }
+        
+        if (isMin)
+        {
+            minB = tmp[i];
+            break;
+        }
+    }
+    
+    cout << minA.first << ' ' << minA.second;
+    cout << '\n';
+    cout << minB.first << ' ' << minB.second;
+    
+    int answer = -1;
     int rank = 1;
-    int minAIdx = 0;
-    int minBIdx = 0;
+    int removed = 0;
+    int lastSum = INF;
     for (int i = 0; i < sums.size(); ++i)
     {
         auto [sum, idx] = sums[i];
         const auto& cur = scores[idx];
-        const auto& minA = scores[sums[minAIdx].second];
-        const auto& minB = scores[sums[minBIdx].second];
-        
         bool noInsen = false;
-        if ( (cur[0] < minA[0] && cur[1] < minA[1])  ||
-             (cur[0] < minB[0] && cur[1] < minB[1])  )
-            noInsen = true;
-        
-        if (!noInsen)
+        if ( (cur[0] < minA.first && cur[1] < minA.second)  ||
+             (cur[0] < minB.first && cur[1] < minB.second)  )
         {
-            if ( (cur[0] < minA[0]) || (cur[0] == minA[0] && cur[1] < minA[1]) ) minAIdx = i;
-            if ( (cur[1] < minB[1]) || (cur[1] == minB[1] && cur[0] < minB[0]) ) minBIdx = i;
+            noInsen = true;
+            ++removed;
+        }
             
-            if (i > 0 && sums[i-1].first != sum)
-                rank = i + 1; 
+        
+        if (!noInsen && lastSum != sum)
+        {
+            rank = i + 1 - removed;
+            lastSum = sum;
         }
         
         if (idx == 0)
         {
-            return noInsen ? -1 : rank;
+            answer = noInsen ? -1 : rank;
         }
     }
+    
+    return answer;
 }
