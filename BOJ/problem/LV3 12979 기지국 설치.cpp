355e61124f2d61031b65919fff47aa7ac2fd5c9b
manbokgo <manbok285@gmail.com>
2024-05-05 22:57:15 +0900

    LV3 12979 구현

---
+++ BOJ/problem/LV3 12979 기지국 설치.cpp
@@ -0,0 +1,31 @@
+﻿// URL: https://school.programmers.co.kr/learn/courses/30/lessons/12979
+// Algo: 구현
+
+// Start:	240505 22 36
+// Read:	0 2
+// Think:	0 5
+// Code:	0 9
+// Total:	0 16
+
+#include <vector>
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
+    stations.push_back(n + w + 1);
+    const int sz = w * 2 + 1;
+    
+    int answer = 0;
+    int cur = 1;
+    for (const int pos : stations)
+    {
+        int frnt = pos - w;
+        if (cur < frnt) {
+            answer += (frnt - cur) / sz;
+            if ((frnt - cur) % sz) ++answer;   
+        }
+        cur = pos + w + 1;
+    }
+
+    return answer;
+}
