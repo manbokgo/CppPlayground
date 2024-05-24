8b19373ba0826b6edc4121d988726a3dda2a8136
manbokgo <manbok285@gmail.com>
2024-05-25 01:47:16 +0900

    G1 13459 시뮬레이션, BFS

---
+++ BOJ/problem/G1 13459 구슬 탈출.cpp
@@ -0,0 +1,169 @@
+﻿// URL: https://www.acmicpc.net/problem/13459
+// Algo: 시뮬레이션, BFS
+
+// Start:	240525 01 44
+// Read:	0 1
+// Think:	0 0
+// Code:	0 1
+// Total:	0 2
+
+// G1 13460 구슬 탈출 2 복붙 날먹
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+constexpr int INF = 0x3f3f3f3f;
+
+using pii = pair<int, int>;
+using tiiii = tuple<int, int, int, int>;
+
+
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0}; // 동남서북
+
+int tile[10][10];
+bool visited[10][10][10][10]; // ry rx by bx
+int main()
+{
+    fastio;
+
+    int n, m;
+    cin >> n >> m;
+
+    tiiii pos;
+    {
+        auto& [ry, rx, by, bx] = pos;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                char c;
+                cin >> c;
+                switch (c)
+                {
+                case '#':
+                    tile[i][j] = -1;
+                    break;
+                case 'O':
+                    tile[i][j] = INF;
+                    break;
+                case 'R':
+                    ry = i;
+                    rx = j;
+                    break;
+                case 'B':
+                    by = i;
+                    bx = j;
+                    break;
+                default: ;
+                }
+            }
+        }
+        visited[ry][rx][by][bx] = true;
+    }
+
+    queue<tiiii> q;
+    q.push(pos);
+
+    int cnt = 1;
+    while (!q.empty())
+    {
+        int qsz = q.size();
+        while (qsz--)
+        {
+            const auto [ry, rx, by, bx] = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; ++i)
+            {
+                int nry = ry;
+                int nrx = rx;
+                int nby = by;
+                int nbx = bx;
+
+                bool rStuck = false;
+                bool bStuck = false;
+                bool rExit = false;
+                bool bExit = false;
+                while (!rStuck || !bStuck)
+                {
+                    if (!rStuck)
+                    {
+                        nry += dy[i];
+                        nrx += dx[i];
+                        if (tile[nry][nrx] == -1)
+                        {
+                            nry -= dy[i];
+                            nrx -= dx[i];
+                            rStuck = true;
+                        }
+                        else if (tile[nry][nrx] == INF)
+                        {
+                            rStuck = true;
+                            rExit = true;
+                        }
+                    }
+
+                    if (!bStuck)
+                    {
+                        nby += dy[i];
+                        nbx += dx[i];
+                        if (tile[nby][nbx] == -1)
+                        {
+                            nby -= dy[i];
+                            nbx -= dx[i];
+                            bStuck = true;
+                        }
+                        else if (tile[nby][nbx] == INF)
+                        {
+                            bStuck = true;
+                            bExit = true;
+                        }
+                    }
+
+                    if (nry == nby && nrx == nbx)
+                    {
+                        if (bStuck)
+                        {
+                            nry -= dy[i];
+                            nrx -= dx[i];
+                            rStuck = true;
+                        }
+                        else if (rStuck)
+                        {
+                            nby -= dy[i];
+                            nbx -= dx[i];
+                            bStuck = true;
+                        }
+                    }
+                }
+
+                if (bExit)
+                {
+                    continue;
+                }
+                if (rExit)
+                {
+                    cout << 1;
+                    return 0;
+                }
+
+                if (!visited[nry][nrx][nby][nbx])
+                {
+                    visited[nry][nrx][nby][nbx] = true;
+                    q.push({nry, nrx, nby, nbx});
+                }
+            }
+        }
+        ++cnt;
+        if (cnt > 10)
+            break;
+    }
+
+    cout << 0;
+    return 0;
+}
