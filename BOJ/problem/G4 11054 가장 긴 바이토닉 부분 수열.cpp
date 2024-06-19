582641416ad74453e6923bc5a4b47b86ce75a15a
manbokgo <manbok285@gmail.com>
2024-06-19 14:27:46 +0900

    G4 11054 DP (LIS+LDS 응용)

---
+++ BOJ/problem/G4 11054 가장 긴 바이토닉 부분 수열.cpp
@@ -0,0 +1,51 @@
+﻿// URL: https://www.acmicpc.net/problem/11054
+// Algo: DP (LIS+LDS 응용)
+
+// Start:	240619 14 03
+// Read:	0 3
+// Think:	0 17
+// Code:	0 4
+// Total:	0 24
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+constexpr int INF = 0x3f3f3f3f;
+
+int arr[1000];
+int dp[1000];
+int front[1000];
+int back[1000];
+
+int main()
+{
+    fastio;
+    memset(dp, 0x3f, sizeof(dp));
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+        int idx = lower_bound(begin(dp), end(dp), arr[i]) - begin(dp);
+        front[i] = idx + 1;
+        dp[idx] = arr[i];
+    }
+
+    memset(dp, 0x3f, sizeof(dp));
+    for (int i = n - 1; i >= 0; --i)
+    {
+        int idx = lower_bound(begin(dp), end(dp), arr[i]) - begin(dp);
+        back[i] = idx + 1;
+        dp[idx] = arr[i];
+    }
+
+    int answer = 0;
+    for (int i = 0; i < n; ++i)
+        answer = max(answer, front[i] + back[i] - 1);
+    cout << answer;
+}
