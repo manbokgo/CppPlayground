3c8aa5f5520828658c9afb129fbc9916de31b483
manbokgo <manbok285@gmail.com>
2024-07-02 12:20:02 +0900

    22963 DP 풀이 추가

--- BOJ/problem/G1 22963 3초 정렬.cpp
+++ BOJ/problem/G1 22963 3초 정렬.cpp
@@ -7,6 +7,15 @@
 // Code:	0 46
 // Total:	0 51
 
+// 일반적인 LIS와 달리 이 문제에선 8 8 8 도 유효한 오름차순 수열이기 때문에
+// lower_bound가 아니라 upper_bound를 써야 한다
+
+// LIS의 길이가 n과 3 넘게 차이난다면 무조건 불가능함
+// LIS를 복원하는 과정에서 스킵되는 것들이 수정되는 것들이다
+
+// + https://blog.naver.com/jinhan814/222900789618
+// DP 풀이 꽤 재밌다. 시간은 비슷한데 메모리는 3배 씀
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -65,3 +74,60 @@ int main()
         }
     }
 }
+
+// DP 풀이
+// https://blog.naver.com/jinhan814/222900789618
+/*
+constexpr int INF = 0x3f3f3f3f;
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+    vector<int> arr(n + 1);
+    for (int i = 1; i <= n; ++i)
+        cin >> arr[i];
+
+    vector<vector<int>> dp(n + 1, vector<int>(4));
+    dp[1][0] = arr[1];
+    dp[1][1] = dp[1][2] = dp[1][3] = 1;
+
+    for (int i = 2; i <= n; ++i)
+    {
+        dp[i][0] = dp[i - 1][0] <= arr[i] ? arr[i] : INF;
+        for (int j = 1; j <= 3; ++j)
+        {
+            dp[i][j] = dp[i - 1][j - 1];
+            if (dp[i - 1][j] <= arr[i])
+                dp[i][j] = min(dp[i][j], arr[i]);
+        }
+    }
+
+    int mn = INF;
+    int idx = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if (mn > dp[n][i])
+        {
+            mn = dp[n][i];
+            idx = i;
+        }
+    }
+
+    if (mn == INF)
+    {
+        cout << "NO";
+        return 0;
+    }
+
+    cout << "YES" << '\n' << idx << '\n';
+
+    for (int i = n, j = idx; i >= 1 && j; --i)
+    {
+        if (dp[i - 1][j] > arr[i] || dp[i - 1][j - 1] < arr[i])
+            cout << i << ' ' << dp[i][j--] << '\n';
+    }
+}
+*/
+
