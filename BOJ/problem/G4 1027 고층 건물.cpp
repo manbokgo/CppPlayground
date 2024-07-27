f309e3db0e7f701b05548f0526f271ce047f3a67
manbokgo <manbok285@gmail.com>
2024-07-27 17:42:14 +0900

    G4 1027 브루트포스, 기하학

---
+++ BOJ/problem/G4 1027 고층 건물.cpp
@@ -0,0 +1,45 @@
+﻿// URL: https://www.acmicpc.net/problem/1027
+// Algo: 브루트포스, 기하학
+
+// Start:	240727 17 16
+// Read:	0 2
+// Think:	0 18
+// Code:	0 4
+// Total:	0 24
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+int arr[50];
+int cnt[50];
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+
+    for (int i = 0; i < n; ++i)
+    {
+        double mx = -5e9;
+        for (int j = i + 1; j < n; ++j)
+        {
+            double slope = (double)(arr[j] - arr[i]) / (j - i);
+            if (mx < slope)
+            {
+                mx = slope;
+                ++cnt[i];
+                ++cnt[j];
+            }
+        }
+    }
+
+    cout << *max_element(cnt, cnt + n);
+}
