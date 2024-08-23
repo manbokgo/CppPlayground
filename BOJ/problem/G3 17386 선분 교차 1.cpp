02e2b73d526cf068f50d3fd3f427454a73a88e23
manbokgo <manbok285@gmail.com>
2024-08-23 17:46:46 +0900

    CCW 코드 수정

--- BOJ/problem/G3 17386 선분 교차 1.cpp
+++ BOJ/problem/G3 17386 선분 교차 1.cpp
@@ -24,22 +24,18 @@ struct Line
     Pos p2;
 };
 
-ll CCW(Pos p1, Pos p2, Pos p3)
-{
-    // return (p2.X - p1.X) * (p3.Y - p1.Y) - (p3.X - p1.X) * (p2.Y - p1.Y);
-    ll ret = (p2.X - p1.X) * (p3.Y - p1.Y) - (p3.X - p1.X) * (p2.Y - p1.Y);
-    return clamp(ret, -1LL, 1LL);
-}
+ll CCW(Pos p1, Pos p2, Pos p3) { return (p2.X - p1.X) * (p3.Y - p1.Y) - (p3.X - p1.X) * (p2.Y - p1.Y); }
+int Normalize(ll var) { return var > 0 ? 1 : var < 0 ? -1 : 0; }
 
 bool LineCrossed(Line l1, Line l2)
 {
-    const ll ret1 = CCW(l1.p1, l1.p2, l2.p1) * CCW(l1.p1, l1.p2, l2.p2);
-    const ll ret2 = CCW(l2.p1, l2.p2, l1.p1) * CCW(l2.p1, l2.p2, l1.p2);
+    const int ret1 = Normalize(CCW(l1.p1, l1.p2, l2.p1)) * Normalize(CCW(l1.p1, l1.p2, l2.p2));
+    const int ret2 = Normalize(CCW(l2.p1, l2.p2, l1.p1)) * Normalize(CCW(l2.p1, l2.p2, l1.p2));
 
     if (ret1 > 0 || ret2 > 0) return false;
     if (ret1 < 0 || ret2 < 0) return true;
 
-    // 일직선 ret1 == 0 && ret2 == 0
+    // 일직선 혹은 한 점 ret1 == 0 && ret2 == 0
     if (l1.p1 > l1.p2) swap(l1.p1, l1.p2);
     if (l2.p1 > l2.p2) swap(l2.p1, l2.p2);
 
