9c76a1af6a6f5f3139d9642155b78809c16e7edb
manbokgo <manbok285@gmail.com>
2024-08-23 19:09:40 +0900

    선분 교차 코드 수정

--- BOJ/problem/G2 17387 선분 교차 2.cpp
+++ BOJ/problem/G2 17387 선분 교차 2.cpp
@@ -29,13 +29,18 @@ int Normalize(ll var) { return var > 0 ? 1 : var < 0 ? -1 : 0; }
 
 bool LineCrossed(Line l1, Line l2)
 {
-    const int ret1 = Normalize(CCW(l1.p1, l1.p2, l2.p1)) * Normalize(CCW(l1.p1, l1.p2, l2.p2));
-    const int ret2 = Normalize(CCW(l2.p1, l2.p2, l1.p1)) * Normalize(CCW(l2.p1, l2.p2, l1.p2));
+    const ll a = CCW(l1.p1, l1.p2, l2.p1);
+    const ll b = CCW(l1.p1, l1.p2, l2.p2);
+    const ll c = CCW(l2.p1, l2.p2, l1.p1);
+    const ll d = CCW(l2.p1, l2.p2, l1.p2);
+
+    const int ret1 = Normalize(a) * Normalize(b);
+    const int ret2 = Normalize(c) * Normalize(d);
 
     if (ret1 > 0 || ret2 > 0) return false;
     if (ret1 < 0 || ret2 < 0) return true;
 
-    // 일직선 혹은 한 점 ret1 == 0 && ret2 == 0
+    // 일직선상 혹은 끝점 겹침 ret1 == 0 && ret2 == 0
     if (l1.p1 > l1.p2) swap(l1.p1, l1.p2);
     if (l2.p1 > l2.p2) swap(l2.p1, l2.p2);
 
