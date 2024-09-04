b9e5ac5a84f808ddba591d4ec52e4337842991cb
manbokgo <manbok285@gmail.com>
2024-09-04 18:10:57 +0900

    상수버퍼 레이아웃 테스트

--- CppPlayground/alignas.cpp
+++ CppPlayground/alignas.cpp
@@ -1,44 +1,34 @@
-﻿struct XMFLOAT4
-{
-    float data[4];
-};
-struct XMFLOAT3
-{
-    float data[3];
-};
-struct XMFLOAT2
-{
-    float data[2];
-};
+﻿#include <directxmath.h>
+using namespace DirectX;
 
-// Good
-#pragma pack(16)
-struct alignas(16) Foo
+struct CMatrices
 {
-    XMFLOAT3 a; // 0
-    XMFLOAT3 b; // 12
-    bool     c; // 24
-    float    d; // 25
-}; // 이처럼 pack이 원래의 정렬보다 더 작다면 효과가 있다.
+    XMFLOAT2 before;          // Offset: 0
+    alignas(16) XMFLOAT3 _11; // Offset: 16
+    alignas(16) XMFLOAT3 _21; // Offset: 32
+    alignas(16) XMFLOAT3 _31; // Offset: 48
+    float smol_mat;        // Offset: 60
+};  // Size: 80  alignof: 16
 
 #include <iostream>
 
 int main()
 {
-    Foo x, y, z;
+    CMatrices x, y, z;
     std::cout
-        << "sizeof(Foo) = " << sizeof(Foo) << '\n'
-        << "alignof(Foo) = " << alignof(Foo) << '\n'
-        << std::hex << std::showbase
-        << "&x: " << &x << '\n'
-        << "&y: " << &y << '\n'
-        << "&z: " << &z << '\n'
+        << "sizeof = " << sizeof(CMatrices) << '\n'
+        << "alignof = " << alignof(CMatrices) << '\n'
+        // << std::hex << std::showbase
+        // << "&x: " << &x << '\n'
+        // << "&y: " << &y << '\n'
+        // << "&z: " << &z << '\n'
         << std::dec << std::showbase
-        << "&diff: " << (int)&z - (int)&y << '\n'
-        << "&z a: " << offsetof(Foo, a) << '\n'
-        << "&z b: " << offsetof(Foo, b) << '\n'
-        << "&z b: " << offsetof(Foo, c) << '\n'
-        << "&z b: " << offsetof(Foo, d) << '\n'
+        // << "&diff: " << (int)&z - (int)&y << '\n'
+        << "1: " << offsetof(CMatrices, before) << '\n'
+        << "2: " << offsetof(CMatrices, _11) << '\n'
+        << "2: " << offsetof(CMatrices, _21) << '\n'
+        << "2: " << offsetof(CMatrices, _31) << '\n'
+        << "4: " << offsetof(CMatrices, smol_mat) << '\n'
         // << "&z c: " << offsetof(Foo, c) << '\n'
         // << "&z d: " << offsetof(Foo, d) << '\n'
         ;
