145bff706d3b83218609e6980aa6fb0afd6291cf
manbokgo <manbok285@gmail.com>
2024-09-18 08:29:04 +0900

    G4 27172 브루트포스, 에라토스테네스의 체

---
+++ BOJ/problem/G4 27172 수 나누기 게임.cpp
@@ -0,0 +1,47 @@
+﻿// URL: https://www.acmicpc.net/problem/27172
+// Algo: 브루트포스, 에라토스테네스의 체
+
+// Start:	240918 08 19
+// Read:	0 2
+// Think:	0 4
+// Code:	0 3
+// Total:	0 9
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+int arr[100'000];
+int score[1'000'001];
+bool used[1'000'001];
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+        used[arr[i]] = true;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = arr[i] * 2; j <= 1'000'000; j += arr[i])
+        {
+            if (!used[j]) continue;
+            --score[j];
+            ++score[arr[i]];
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << score[arr[i]] << ' ';
+    }
+}
