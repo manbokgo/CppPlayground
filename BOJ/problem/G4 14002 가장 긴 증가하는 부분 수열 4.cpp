f407caa1bbb15b6cfe421d866f7ff1f9d8022140
manbokgo <manbok285@gmail.com>
2024-10-26 08:39:14 +0900

    G4 14002 DP (LIS + 복원)

---
+++ BOJ/problem/G4 14002 가장 긴 증가하는 부분 수열 4.cpp
@@ -0,0 +1,55 @@
+﻿// URL: https://www.acmicpc.net/problem/14002
+// Algo: DP (LIS + 복원)
+
+// Start:	241026 08 37
+// Read:	0 1
+// Think:	0 0
+// Code:	0 0
+// Total:	0 1
+
+// LIS 5 문제 복붙
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+constexpr int INF = 0x3f3f3f3f;
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    vector<int> seq(n);
+    vector<int> indices(n);
+    vector<int> dp(n, INF);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> seq[i];
+        int idx = lower_bound(all(dp), seq[i]) - dp.begin();
+        dp[idx] = seq[i];
+        indices[i] = idx + 1;
+    }
+    const int LIS = lower_bound(all(dp), INF) - dp.begin();
+
+    cout << LIS << '\n';
+
+    vector<int> answer(LIS);
+    int fIdx = LIS;
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (indices[i] == fIdx)
+        {
+            answer[fIdx - 1] = seq[i];
+            --fIdx;
+        }
+    }
+
+    for (const int num : answer) cout << num << ' ';
+}
