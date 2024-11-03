25e05ac2ccd37b681c24caed41e55f82b511684a
manbokgo <manbok285@gmail.com>
2024-11-03 19:01:19 +0900

    9252 LCS 함수화

--- BOJ/problem/G4 9252 LCS 2.cpp
+++ BOJ/problem/G4 9252 LCS 2.cpp
@@ -1,5 +1,5 @@
 ﻿// URL: https://www.acmicpc.net/problem/9252
-// Algo: DP (LCS)
+// Algo: DP (LCS 기본)
 
 // Start:	240312 18 36
 // Read:	0 0
@@ -12,68 +12,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
-#define Y first
-#define X second
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
 #define all(x) x.begin(), x.end()
-#define SortVec(x) sort(all(x))
-#define CompressVec(x) x.erase(unique(all(x)), x.end())
-#define ExistInVec(x, value) find(all(x), value) != x.end()
+#define pb push_back
 
-using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using tiii = tuple<int, int, int>;
-using tlll = tuple<ll, ll, ll>;
+string LCS(const string& a, const string& b)
+{
+    const int mxSz = (int)max(a.size(), b.size()) + 1;
+    vector<vector<int>> dp(mxSz, vector<int>(mxSz));
 
-constexpr int INF = 0x3f3f3f3f;
+    for (int i = 0; i <= (int)a.size(); ++i)
+    {
+        for (int j = 0; j <= (int)b.size(); ++j)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (a[i - 1] == b[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
 
-int LCS[1001][1001];
+    if (dp[a.size()][b.size()] == 0) return {};
+
+    string ret;
+    for (int i = (int)a.length(), j = (int)b.length();;)
+    {
+        const int cur = dp[i][j];
+        if (cur == 0) break;
+
+        if (cur == dp[i - 1][j]) { --i; }
+        else if (cur == dp[i][j - 1]) { --j; }
+        else
+        {
+            ret.pb(a[i - 1]);
+            --i;
+            --j;
+        }
+    }
+    reverse(all(ret));
+
+    return ret;
+}
+
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    fastio;
 
     string aStr, bStr;
     cin >> aStr >> bStr;
 
-    for (int i = 0; i <= aStr.size(); ++i)
-    {
-        for (int j = 0; j <= bStr.size(); ++j)
-        {
-            if (i == 0 || j == 0)
-                LCS[i][j] = 0;
-            else if (aStr[i - 1] == bStr[j - 1])
-                LCS[i][j] = LCS[i - 1][j - 1] + 1;
-            else
-                LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1]);
-        }
-    }
-
-    cout << LCS[aStr.size()][bStr.size()];
-    if (LCS[aStr.size()][bStr.size()] > 0)
-    {
-        cout << "\n";
-
-        string answer;
-
-        for (int i = aStr.length(), j = bStr.length();;)
-        {
-            int cur = LCS[i][j];
-            if (cur == 0) break;
-
-            if (cur == LCS[i - 1][j]) --i;
-            else if (cur == LCS[i][j - 1]) --j;
-            else
-            {
-                answer.pb(aStr[i - 1]);
-                --i;
-                --j;
-            }
-        }
-
-        reverse(all(answer));
-        cout << answer;
-    }
+    const string answer = LCS(aStr, bStr);
+    cout << answer.size() << '\n' << answer;
 }
