00677979e870e78c64e877c066cf6f3a378e3a49
manbokgo <manbok285@gmail.com>
2024-11-16 06:05:14 +0900

    G3 7432 트라이 클래스

---
+++ BOJ/problem/G3 7432 디스크 트리.cpp
@@ -0,0 +1,84 @@
+﻿// URL: https://www.acmicpc.net/problem/7432
+// Algo: 트라이
+
+// Start:	241116 05 56
+// Read:	0 1
+// Think:	0 2
+// Code:	0 5
+// Total:	0 8
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+vector<string> Split(const string& str, const string& delim)
+{
+    size_t st = 0, en;
+    const size_t delimLen = delim.length();
+
+    vector<string> result;
+    while ((en = str.find(delim, st)) != string::npos)
+    {
+        string token = str.substr(st, en - st);
+        st = en + delimLen;
+        result.push_back(token);
+    }
+
+    if (st != str.size())
+        result.push_back(str.substr(st));
+
+    return result;
+}
+
+class Trie
+{
+    bool m_Chk = false;
+    map<string, Trie> m_Map;
+
+public:
+    void Insert(const vector<string>& arr, int idx = 0)
+    {
+        if (idx == arr.size())
+        {
+            m_Chk = true;
+            return;
+        }
+
+        m_Map[arr[idx]].Insert(arr, idx + 1);
+    }
+
+    void Print(int depth = 0) const
+    {
+        for (const auto& [name, node] : m_Map)
+        {
+            for (int i = 0; i < depth; ++i)
+            {
+                cout << " ";
+            }
+            cout << name << '\n';
+            node.Print(depth + 1);
+        }
+    }
+};
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    Trie root;
+    while (n--)
+    {
+        string str;
+        cin >> str;
+
+        root.Insert(Split(str, "\\"));
+    }
+
+    root.Print();
+}
