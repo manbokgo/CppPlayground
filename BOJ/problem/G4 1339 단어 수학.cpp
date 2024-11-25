3b6bb446b70111ab6eafecdbc0271080220ae53e
manbokgo <manbok285@gmail.com>
2024-11-25 10:00:15 +0900

    G4 1339 그리디

---
+++ BOJ/problem/G4 1339 단어 수학.cpp
@@ -0,0 +1,50 @@
+﻿// URL: https://www.acmicpc.net/problem/1339
+// Algo: 그리디
+
+// Start:	241125 09 51
+// Read:	0 1
+// Think:	0 4
+// Code:	0 3
+// Total:	0 8
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+int sum[26];
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    string str;
+    while (n--)
+    {
+        cin >> str;
+
+        int val = 1;
+        for (int i = str.size() - 1; i >= 0; --i)
+        {
+            sum[str[i] - 'A'] += val;
+            val *= 10;
+        }
+    }
+    sort(sum, sum + 26, greater<>());
+
+    int ans = 0;
+    int num = 9;
+    for (int i = 0; i < 26; ++i)
+    {
+        ans += sum[i] * num;
+        --num;
+        if (num == 0) break;
+    }
+
+    cout << ans;
+}
