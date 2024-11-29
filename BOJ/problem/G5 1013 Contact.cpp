732c40273b2ed1d353b58eba67974496e7fecc0a
manbokgo <manbok285@gmail.com>
2024-11-29 11:46:44 +0900

    G5 1013 정규 표현식

---
+++ BOJ/problem/G5 1013 Contact.cpp
@@ -0,0 +1,31 @@
+﻿// URL: https://www.acmicpc.net/problem/1013
+// Algo: 정규 표현식
+
+// Start:	241129 11 40
+// Read:	0 2
+// Think:	0 2
+// Code:	0 2
+// Total:	0 6
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    const regex pattern{"(100+1+|01)+"};
+    string str;
+    while (n--)
+    {
+        cin >> str;
+        cout << (regex_match(str, pattern) ? "YES\n" : "NO\n");
+    }
+}
