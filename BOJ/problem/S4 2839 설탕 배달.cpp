c702125fc738e1be219be3a66f74b64fab401823
manbokgo <manbok285@gmail.com>
2024-11-30 13:16:52 +0900

    S4 2839 그리디

---
+++ BOJ/problem/S4 2839 설탕 배달.cpp
@@ -0,0 +1,37 @@
+﻿// URL: https://www.acmicpc.net/problem/2839
+// Algo: 그리디
+
+// Start:	241130 13 09
+// Read:	0 1
+// Think:	0 1
+// Code:	0 4
+// Total:	0 6
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    while (n >= 0)
+    {
+        if (n % 5 == 0)
+        {
+            ans += n / 5;
+            cout << ans;
+            return 0;
+        }
+        n -= 3;
+        ++ans;
+    }
+    cout << -1;
+}
