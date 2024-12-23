ca7a3b7d96ac30b2f38f2a9f3f31b8d0e1cb8d2d
manbokgo <manbok285@gmail.com>
2024-12-23 09:13:25 +0900

    G5 20157 기하학, 해시 맵

---
+++ BOJ/problem/G5 20157 화살을 쏘자!.cpp
@@ -0,0 +1,42 @@
+﻿// URL: https://www.acmicpc.net/problem/20157
+// Algo: 기하학, 해시 맵
+
+// Start:	241223 09 03
+// Read:	0 2
+// Think:	0 4
+// Code:	0 4
+// Total:	0 10
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+using pii = pair<int, int>;
+
+map<pii, int> slope;
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        const int gc = gcd(x, y);
+        ++slope[{x/ gc, y/ gc}];
+    }
+
+    int ans = 0;
+    for (const auto& [_, cnt] : slope)
+        ans = max(ans, cnt);
+
+    cout << ans;
+}
