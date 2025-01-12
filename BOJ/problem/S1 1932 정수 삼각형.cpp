f0ef2aa5d3dac7e5846d81f4138c5dd6d7f1b8ae
manbokgo <manbok285@gmail.com>
2025-01-13 08:12:43 +0900

    S1 1932 DP

---
+++ BOJ/problem/S1 1932 정수 삼각형.cpp
@@ -0,0 +1,43 @@
+﻿// URL: https://www.acmicpc.net/problem/1932
+// Algo: DP
+
+// Start:	250113 08 04
+// Read:	0 1
+// Think:	0 4
+// Code:	0 3
+// Total:	0 8
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+int dp[501][501];
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            cin >> dp[i][j];
+        }
+    }
+
+    for (int i = n - 1; i >= 1; --i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            dp[i-1][j] += max(dp[i][j], dp[i][j+1]);
+        }
+    }
+
+    cout << dp[0][0];
+}
