45ab784b5437cf3850234779ca29f21c57d7993b
manbokgo <manbok285@gmail.com>
2025-01-15 07:57:42 +0900

    S1 1629 수학

---
+++ BOJ/problem/S1 1629 곱셈.cpp
@@ -0,0 +1,36 @@
+﻿// URL: https://www.acmicpc.net/problem/1629
+// Algo: 수학
+
+// Start:	250115 07 47
+// Read:	0 1
+// Think:	0 5
+// Code:	0 4
+// Total:	0 10
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+using ll = long long;
+
+ll a, b, c;
+
+ll Solve(ll cur)
+{
+    if (cur == 0) return 1;
+    if (cur == 1) return a % c;
+
+    const ll half = Solve(cur / 2) % c;
+
+    if (cur % 2 == 0) return half * half % c;
+    else return half * half % c * a % c;
+}
+
+int main()
+{
+    cin >> a >> b >> c;
+    cout << Solve(b);
+}
