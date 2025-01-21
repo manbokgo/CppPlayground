97725b50eb8faa46d3a4fc589411f91938e1e5f6
manbokgo <manbok285@gmail.com>
2025-01-21 09:47:42 +0900

    G5 1931 그리디

---
+++ BOJ/problem/G5 1931 회의실 배정.cpp
@@ -0,0 +1,44 @@
+﻿// URL: https://www.acmicpc.net/problem/1931
+// Algo: 그리디
+
+// Start:	250121 09 41
+// Read:	0 1
+// Think:	0 3
+// Code:	0 2
+// Total:	0 6
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+using pii = pair<int, int>;
+
+pii arr[100'000];
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i].second >> arr[i].first;
+
+    sort(arr, arr + n);
+
+    int ans = 0;
+    int en = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (en <= arr[i].second)
+        {
+            en = arr[i].first;
+            ans++;
+        }
+    }
+    cout << ans;
+}
