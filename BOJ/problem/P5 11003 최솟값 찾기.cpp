7655106cb0f70487ee180db0fbd4249ebec56388
manbokgo <manbok285@gmail.com>
2025-01-27 15:16:39 +0900

    11003 주석 수정

--- BOJ/problem/P5 11003 최솟값 찾기.cpp
+++ BOJ/problem/P5 11003 최솟값 찾기.cpp
@@ -9,6 +9,7 @@
 
 // LV3 64062 징검다리 건너기 문제에서 흥미로워서 알아보았던 Sliding Window Maximum 방법 O(n)을 이용한 문제
 // https://velog.io/@rmswjdtn/%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%94%A9-%EC%9C%88%EB%8F%84%EC%9A%B0-Leet-code-239.-Sliding-Window-Maximum
+// https://ansohxxn.github.io/boj/11003/
 
 // 1. Deque의 뒤에서부터 원소를 집어넣는다.
 // 2. Deque의 앞 원소가 지금 집어넣을 원소에서 왼쪽으로 k 범위 안에 없다면, pop_front()
